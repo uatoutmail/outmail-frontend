b375b7a8a509e3788b89d12b9ef1c58c4c2ce5a6
Tanmay0215 <tanmaygupta.0215@gmail.com>
2026-06-10 08:23:37 +0400

    add razorpay in csp

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com;
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://checkout.razorpay.com;
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
     img-src 'self' blob: data: https://images.unsplash.com https://lh3.googleusercontent.com https://res.cloudinary.com https://outmail.in.s3.ap-south-1.amazonaws.com;
     font-src 'self' https://fonts.gstatic.com;
-    connect-src 'self' ${process.env.NEXT_PUBLIC_API_BASE_URL || ""} https://www.google-analytics.com;
-    frame-src 'self';
+    connect-src 'self' ${process.env.NEXT_PUBLIC_API_BASE_URL || ""} https://www.google-analytics.com https://api.razorpay.com;
+    frame-src 'self' https://api.razorpay.com;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
